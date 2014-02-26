6257dc485b376855cc01ab1601ebd9de90092419
Oides Andrade <eudes.andrade@gmail.com>
2014-02-26 14:49:46 -0300

    Versão inicial do projeto para testes de push notification

---
+++ Adventure/src/br/gov/serpro/adventure/EventList.java
@@ -0,0 +1,23 @@
+package br.gov.serpro.adventure;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class EventList extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_list);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.event_list, menu);
+        return true;
+    }
+    
+}
