b326e7a7ef473dd911be0243b40719ff23d2f354
Oides Andrade <eudes.andrade@gmail.com>
2014-02-26 14:59:47 -0300

    Refatorando o nome do arquivo de activity.

--- Adventure/src/br/gov/serpro/adventure/EventListActivity.java
+++ Adventure/src/br/gov/serpro/adventure/EventListActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class EventList extends Activity {
+public class EventListActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
